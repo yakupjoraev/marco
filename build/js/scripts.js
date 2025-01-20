af72da471fe89869b642c59bf14ec34403ae572e
Yakup <yakupjoraev@gmail.com>
2025-01-20 16:55:14 +0300

    added new pages

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -47,6 +47,109 @@ function collectionSlider() {
 
 collectionSlider();
 
+function historySlider() {
+  const container = document.querySelector('.history__slider-container');
+
+  if (!container) {
+    return null;
+  }
+
+  const paginationItems = document.querySelectorAll('.history__slider-pagination-item');
+  const paginationSums = document.querySelector('.history__slider-pagination-sums');
+
+  const swiper = new Swiper('.history__slider', {
+    slidesPerView: 1.2,
+    centeredSlides: true,
+    spaceBetween: 32,
+    effect: 'coverflow',
+    coverflowEffect: {
+      rotate: 0,
+      depth: 200,
+      stretch: 30,
+      modifier: 1,
+      scale: 0.8,
+      slideShadows: false,
+    },
+    on: {
+      init: () => {
+        updatePagination(0);
+        updatePaginationSums(0);
+      }, // При инициализации делаем первый элемент активным
+      slideChange: () => {
+        updatePagination(swiper.realIndex);
+        updatePaginationSums(swiper.realIndex);
+      }, // Обновляем активный элемент при смене слайда
+    },
+  });
+
+  paginationItems.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      swiper.slideTo(index); // При клике переходим к соответствующему слайду
+    });
+  });
+
+  function updatePagination(activeIndex) {
+    paginationItems.forEach((item, index) => {
+      if (index === activeIndex) {
+        item.classList.add('active');
+      } else {
+        item.classList.remove('active');
+      }
+    });
+  }
+
+  function updatePaginationSums(activeIndex) {
+    if (paginationSums) {
+      paginationSums.textContent = `${activeIndex + 1} из ${paginationItems.length}`;
+    }
+  }
+}
+
+historySlider();
+
+
+function historyReadSlide() {
+  const container = document.querySelector('.history');
+
+  if (!container) {
+    return null;
+  }
+
+  // Находим кнопку для открытия и закрытия контента
+  const btnRead = document.querySelector(".history__btn-read");
+  const slider = document.querySelector(".history__slider");
+
+  if (btnRead && slider) {
+    btnRead.addEventListener("click", () => {
+      const activeSlide = slider.querySelector(".swiper-slide-active");
+
+      if (activeSlide) {
+        const slideInfo = activeSlide.querySelector(".history__slide-info");
+        if (slideInfo) {
+          slideInfo.classList.toggle("visible");
+        }
+      }
+    });
+
+    // Находим все кнопки закрытия слайдов
+    const closeButtons = slider.querySelectorAll(".history__slide-btn");
+    closeButtons.forEach((closeButton) => {
+      closeButton.addEventListener("click", () => {
+        const activeSlide = slider.querySelector(".swiper-slide-active");
+
+        if (activeSlide) {
+          const slideInfo = activeSlide.querySelector(".history__slide-info");
+          if (slideInfo) {
+            slideInfo.classList.remove("visible");
+          }
+        }
+      });
+    });
+  }
+}
+
+historyReadSlide()
+
 
 
 const openModalBtns = document.querySelectorAll('.open-modal-btn');
